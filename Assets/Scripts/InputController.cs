88dd96f826abb2b697b1ab6235a37d0c0c1b5534
Mieke.Narjes <mieke.narjes@haw-hamburg.de>
2018-02-24 20:59:37 +0100

    added lightsOff and menu connection

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class InputController : MonoBehaviour {
 
     private static InputController instance;
+    public bool IsVisible = true;
+    public GameObject level;
+
     public static InputController getInstance()
     {
         return instance;
@@ -20,18 +23,41 @@ public class InputController : MonoBehaviour {
 
     public float getAxis(int player, string axis)
     {
-        return Input.GetAxis(player + axis);
+        float res = Input.GetAxis(player + axis);
+        if(res != 0)
+        {
+            MakeInvisible();
+        }
+        return res;
     }
 
     public bool getButtonDown(int player, KeyCode button, string key)
     {
-        return Input.GetKeyDown((KeyCode)(player * 20) + (int)button) || Input.GetButtonDown(player + key);
+        bool res = Input.GetKeyDown((KeyCode)(player * 20) + (int)button) || Input.GetButtonDown(player + key);
+        if (res)
+        {
+            MakeInvisible();
+        }
+        return res;
     }
 
     public bool getButtonUp(int player, KeyCode button, string key)
     {
-        return Input.GetKeyUp((KeyCode)(player * 20) + (int)button) || Input.GetButtonDown(player + key);
+        bool res = Input.GetKeyUp((KeyCode)(player * 20) + (int)button) || Input.GetButtonDown(player + key);
+        if (res)
+        {
+            MakeInvisible();
+        }
+        return res;
     }
 
+    public void MakeInvisible()
+    {
+        level.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 0f);
+    }
 
+    public void MakeVisible()
+    {
+        level.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+    }
 }
