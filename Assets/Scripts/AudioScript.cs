25880b9df74929013256288fe79d5e566bf2cd99
Mieke.Narjes <mieke.narjes@haw-hamburg.de>
2018-02-24 21:39:36 +0100

    final

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    private bool wasJumping = false;
+    private bool wasAlive = false;
+    private Animator animator;
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+
+        if (animator.GetBool("isJumping") && !wasJumping)
+        {
+            MusicSource.Play();
+            Debug.Log("jump");
+        }
+
+  
+        wasJumping = animator.GetBool("isJumping");
+        wasAlive = animator.GetBool("isAlive");
+    }
+}
---
+++ Assets/Scripts/BGMusic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusic : MonoBehaviour {
+
+    private Animator animator;
+    public AudioClip MusicClipBGMusic;
+    public AudioSource MusicSourceBGMusic;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        MusicSourceBGMusic.clip = MusicClipBGMusic;
+
+        MusicSourceBGMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
